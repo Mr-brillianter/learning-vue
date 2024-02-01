79a13e653409613777a7379b688b50a441ca0f7f
Mr.brillianter <357021376@qq.com>
2024-02-01 10:10:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script >
+import comp_test from "./components/comp_test.vue";
+
+export default{
+  data(){
+    return{
+      msg:'神奇的语法'+'sad',
+      
+    }
+  },
+  components:{comp_test}
+}
+</script>
+
+<template>
+<p>{{ msg}}</p>
+<comp_test/>
+</template>
+
+<style >
+
+</style>
---
+++ src/components/comp_test.vue
@@ -0,0 +1,20 @@
+<script >
+export default{
+  data(){
+    return{
+      msg2:'这是一个组件'
+    }
+  }
+}
+</script>
+
+<template>
+<div class="myclassfont1">{{ msg2}}</div>
+</template>
+
+<style >
+.myclassfont1{
+  font-size: 12px;
+  color:blueviolet
+}
+</style>
