78289b0ef110bc941425244a3633ea970194d254
Mr.brillianter <357021376@qq.com>
2024-02-01 21:54:10 +0800

    v-on

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script >
 // import vif from "./components/vif.vue";
-import vfor from "./components/vfor.vue";
+// import vfor from "./components/vfor.vue";
+import von from "./components/von.vue";
 export default{
-  components:{vfor}
+  components:{von}
 }
 </script>
 
 <template>
-<vfor/>
+<von/>
 </template>
 
 
---
+++ src/components/von.vue
@@ -0,0 +1,15 @@
+<template>
+<button v-on:click="count++">{{ count }}这里</button>
+<div>{{ count }}这里</div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            count:0
+        }
+    }
+}
+</script>
+
