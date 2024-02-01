df64ae6efe8ffc4a27b3a2041b002d3851045bcf
Mr.brillianter <357021376@qq.com>
2024-02-01 22:10:26 +0800

    v-on2

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 // import vif from "./components/vif.vue";
 // import vfor from "./components/vfor.vue";
 import von from "./components/von.vue";
+import von2 from "./components/von2.vue";
 export default{
-  components:{von}
+  components:{von,von2}
 }
 </script>
 
 <template>
 <von/>
+<von2/>
 </template>
 
 
---
+++ src/components/von2.vue
@@ -0,0 +1,19 @@
+<template>
+<button v-on:click="AddCount">{{ count }}这里</button>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            count:0
+        }
+    },
+    methods:{
+        AddCount(){
+        this.count++
+    }
+    }
+}
+</script>
+
