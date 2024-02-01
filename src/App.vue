a0e8e82070f44d7fc83b9613217625b6279c8abb
Mr.brillianter <357021376@qq.com>
2024-02-01 11:57:40 +0800

    if-elif

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script >
-import comp_test from "./components/comp_test.vue";
 import vif from "./components/vif.vue";
 export default{
-  data(){
-    return{
-      msg:'神奇的语法'+'sad',
-      
-    }
-  },
-  components:{comp_test,vif}
+  components:{vif}
 }
 </script>
 
 <template>
-<p>{{ msg}}</p>
 <vif/>
-<comp_test/>
 </template>
 
 <style >
--- src/components/vif.vue
+++ src/components/vif.vue
@@ -1,15 +1,16 @@
 <template>
-<h1>
-    hello!
-</h1>
-<div v-if="flag">v-if条件判断是否显示</div>
+<div v-if="type=='A'">类型是A</div>
+<div v-else-if="type=='B'">类型是B</div>
+<div v-else-if="type=='C'">类型是C</div>
+<div v-else>类型不是ABC</div>
 
 </template>
 
 <script>
 export default{
     data(){
-        return{flag:1
+        return{
+            type:'d'
         }
     }
 }
